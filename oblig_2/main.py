7af1f91b82b0c63f84e37848ef43a9f9658abf24
78-nour-78 <nohaj5878@oslomet.no>
2026-03-20 20:07:10 +0100

    siste endringer med grafen

---
+++ oblig_2/main.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+
+# Likningen vi løser (for x)
+def likning(x):
+    return np.arctan(x) - 4/(x**2 +1)
+
+# Original funksjon
+def f(x):
+    return np.exp(-x/4) * np.arctan(x)
+
+# Startverdi
+x0 = 0.2
+
+# Løs likning
+løsning = fsolve(likning, x0)
+x = løsning[0]
+
+# Finn y-verdi
+y = f(x)
+
+# Print med 4 desimaler ✅
+print(f"x = {x:.4f}")
+print(f"y = {y:.4f}")
+
+# Lage graf
+x_vals = np.linspace(0, 5, 100)
+y_vals = f(x_vals)
+
+plt.plot(x_vals, y_vals)
+plt.scatter(x, y)  # toppunkt
+
+plt.title("Graf av f(x)")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+
+plt.savefig("plot.png")
+plt.show()
+
